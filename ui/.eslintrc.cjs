cd9530994c0dba98bed77d2bba827b7b92f3d3db
Wazarr <jeje_04@live.com>
2023-05-08 13:59:18 +0200

    fix eslint

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -2,13 +2,20 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier"
-  ],
-  "env": {
-    "vue/setup-compiler-macros": true
-  }
-}
+	root: true,
+	env: {
+		node: true,
+		"vue/setup-compiler-macros": true,
+	},
+	extends: [
+		"plugin:vue/vue3-essential",
+		"eslint:recommended",
+		"@vue/eslint-config-prettier",
+		"@vue/typescript/recommended",
+	],
+	parserOptions: {
+		ecmaVersion: 2020,
+		jsx: true,
+		tsx: true,
+	},
+};
