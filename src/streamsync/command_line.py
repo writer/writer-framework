6458cfa5c137ffdd433e4367e8cfa22cbb2c35f3
Ramiro Medina <ramiro.a.medina@gmail.com>
2023-07-03 22:42:46 +0000

    Pyarrow type ignore

--- src/streamsync/command_line.py
+++ src/streamsync/command_line.py
@@ -49,7 +49,7 @@ def _perform_checks(command: str, absolute_app_path: str, host: Optional[str]):
     if command in ("hello"):
         try:
             import pandas
-            import pyarrow
+            import pyarrow # type: ignore
             import plotly.express # type: ignore
         except ImportError:
             logging.error("Running streamsync hello requires pandas, pyarrow and plotly.express. Install them with:\npip install streamsync[ds]")
