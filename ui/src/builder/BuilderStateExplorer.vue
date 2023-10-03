5d9848833d21bf065a0a5e341ce59a89852a2db4
Ramiro Medina <ramiro.a.medina@gmail.com>
2023-10-03 19:51:38 +0000

    State explorer

---
+++ ui/src/builder/BuilderStateExplorer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="BuilderStateExplorer">
+        <BuilderStateExplorerTreeBranch :root-accessors="[]"></BuilderStateExplorerTreeBranch>
+    </div>
+</template>
+
+<script setup lang="ts">
+import BuilderStateExplorerTreeBranch from './BuilderStateExplorerTreeBranch.vue';
+
+</script>
+
+<style scoped>
+@import "./sharedStyles.css";
+
+.BuilderStateExplorer {
+    max-height: 70vh;
+    overflow-y: auto;
+}
+
+</style>
