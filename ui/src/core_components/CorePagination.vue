eaa2ff95dea565f8056c82121aef0b709a7788ba
Fabien Arcellier <fabien.arcellier@gmail.com>
2023-11-12 18:12:26 +0100

    build a component to paginate results
    * feat: declare the property on pagination component

--- ui/src/core_components/CorePagination.vue
+++ ui/src/core_components/CorePagination.vue
@@ -9,7 +9,59 @@ export default {
 	streamsync: {
 		name: "Pagination",
 		category: "Other",
-		fields: {},
+		description: "paginates records from a repeater or dataframe",
+		fields: {
+			page: {
+				name: "Page",
+				init: "1",
+				type: FieldType.Number,
+				desc: "The current page number.",
+			},
+			pageSize: {
+				name: "Page Size",
+				init: "25",
+				type: FieldType.Number,
+				desc: "The number of records per page.",
+			},
+			totalItems: {
+				name: "Total Items",
+				init: "0",
+				type: FieldType.Number,
+				desc: "The total number of records",
+			},
+			pageSizeOptions: {
+				name: "Page Size Options",
+				init: "25,50,100",
+				type: FieldType.Text,
+				desc: "A comma-separated list of page size options.",
+			},
+			pageSizeShowAll: {
+				name: "Show All Option",
+				init: "no",
+				type: FieldType.Text,
+				options: {
+					yes: "Yes",
+					no: "No",
+				},
+				desc: "Show an option to show all records.",
+			},
+			jumpTo: {
+				name: "Jump To",
+				init: "no",
+				type: FieldType.Text,
+				options: {
+					yes: "Yes",
+					no: "No",
+				},
+				desc: "Show an option to jump to a specific page.",
+			},
+			urlParam: {
+				name: "URL Param",
+				init: "",
+				type: FieldType.Text,
+				desc: "The URL parameter to use for the page number. If it's empty, the page is not saved on the url.",
+			},
+		},
 		events: {}
 	}
 };
