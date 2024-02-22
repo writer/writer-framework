29394fed4e6d5ddcaba750ce7e03ff76002ab367
gunnar-enerin <122986045+gunnar-enerin@users.noreply.github.com>
2024-02-22 12:09:34 +0100

    Add square barckets to evaluator regex

--- src/streamsync/core.py
+++ src/streamsync/core.py
@@ -743,7 +743,7 @@ class Evaluator:
     It allows for the sanitisation of frontend inputs.
     """
 
-    template_regex = re.compile(r"[\\]?@{([\w\s.]*)}")
+    template_regex = re.compile(r"[\\]?@{([\w\s.\[\]]*)}")
 
     def __init__(self, session_state: StreamsyncState, session_component_tree: ComponentTree):
         self.ss = session_state
