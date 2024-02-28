c7006570b713d159fc2c9e3971deb9be76099499
Mateusz Russak <mateusz@russak.biz>
2024-02-28 15:44:31 +0100

    chore: changed description of the link component

--- ui/src/core_components/content/CoreLink.vue
+++ ui/src/core_components/content/CoreLink.vue
@@ -24,7 +24,7 @@ export default {
 				name: "URL",
 				type: FieldType.Text,
 				default: "https://streamsync.cloud",
-				desc: "A valid URL.",
+				desc: "Specify a URL or choose a page. Keep in mind that you can only link to pages for which a key has been specified.",
 				options: (ss: Core) => {
 					return Object.fromEntries(
 						ss
