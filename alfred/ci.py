5e2b52187eaf41bad7e1d9b89a9f7d27abc1766a
Mateusz Russak <mateusz@russak.biz>
2024-03-01 14:37:16 +0100

    chore: trying separate gh action for playwright

--- alfred/ci.py
+++ alfred/ci.py
@@ -6,7 +6,6 @@ import alfred
 @alfred.command("ci", help="continuous integration pipeline")
 @alfred.option('--front', '-f', help="run for frontend only", is_flag=True, default=False)
 @alfred.option('--back', '-b', help="run for backend only", is_flag=True, default=False)
-@alfred.option('--e2e', '-e', help="run e2e tests", is_flag=True, default=False)
 def ci(front, back, e2e):
     if back or (not front and not back and not e2e):
         alfred.invoke_command("ci.mypy")
@@ -14,8 +13,6 @@ def ci(front, back, e2e):
     if front or (not front and not back and not e2e):
         alfred.invoke_command("npm.lint")
         alfred.invoke_command("npm.build")
-    if e2e or (not front and not back and not e2e):
-        alfred.invoke_command("npm.e2e")
 
 @alfred.command("ci.mypy", help="typing checking with mypy on ./src/streamsync")
 def ci_mypy():
