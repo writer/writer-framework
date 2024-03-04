379ea511994f81e15432eeab52d8e9794726f89f
Mateusz Russak <mateusz@russak.biz>
2024-03-04 13:23:41 +0100

    chore: fixed failing tests

--- e2e_tests/playwright.config.ts
+++ e2e_tests/playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
 	testDir: "./tests",
 	fullyParallel: false,
 	forbidOnly: !!process.env.CI,
-	retries: process.env.CI ? 2 : 0,
+	retries: 0,
 	workers: 1,
 	reporter: "list",
 	use: {
