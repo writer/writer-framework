f4f8b2e8b274286464251ec7af11291ffe7a07b5
Ramiro Medina <64783088+ramedina86@users.noreply.github.com>
2024-09-27 14:13:52 +0000

    chore: Handle delete

--- src/ui/src/components/workflows/base/WorkflowArrow.vue
+++ src/ui/src/components/workflows/base/WorkflowArrow.vue
@@ -1,78 +1,96 @@
 <template>
-	<g tabindex="0" @click="handleClick">
-		<path :d="pathD" :style="{ stroke: props.arrow.color }"></path>
+	<g class="WorkflowArrow" tabindex="0" data-writer-unselectable="true">
+		<path :d="pathD" :style="{ stroke: arrow.color }"></path>
 		<path
 			:d="pathD"
 			:style="{
-				stroke: props.arrow.color,
+				stroke: arrow.color,
 				strokeWidth: 12,
 				opacity: isSelected ? `0.2` : `0`,
 			}"
 		></path>
+		<g v-if="isSelected" class="delete" @click="handleDeleteClick">
+			<circle :cx="points[3].x" :cy="points[3].y" r="12" />
+			<line
+				class="cross"
+				:x1="points[3].x - 4"
+				:y1="points[3].y - 4"
+				:x2="points[3].x + 4"
+				:y2="points[3].y + 4"
+				:stroke="arrow.color"
+				stroke-width="2"
+			/>
+			<line
+				class="cross"
+				:x1="points[3].x + 4"
+				:y1="points[3].y - 4"
+				:x2="points[3].x - 4"
+				:y2="points[3].y + 4"
+				:stroke="arrow.color"
+				stroke-width="2"
+			/>
+		</g>
 	</g>
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from "vue";
+import { type WorkflowArrowData } from "../WorkflowsWorkflow.vue";
+
+const PROTRUSION_LENGTH = 30;
+const emit = defineEmits(["delete"]);
 
 const props = defineProps<{
-	arrow: {
-		x1: number;
-		y1: number;
-		x2: number;
-		y2: number;
-		color: string;
-	};
+	arrow: WorkflowArrowData;
+	isSelected: boolean;
 }>();
 
-const isSelected = ref(false);
-
-function handleClick() {
-	isSelected.value = true;
-}
-
-const pathD = computed(() => {
+const points = computed(() => {
 	const xDif = props.arrow.x2 - props.arrow.x1;
 	const yDif = props.arrow.y2 - props.arrow.y1;
 
-	const protrusionLength = 30;
-
-	const points = [
+	const ps = [
 		{ x: props.arrow.x1, y: props.arrow.y1 },
-		{ x: props.arrow.x1 + protrusionLength, y: props.arrow.y1 },
+		{ x: props.arrow.x1 + PROTRUSION_LENGTH, y: props.arrow.y1 },
 		{
-			x: props.arrow.x1 + protrusionLength,
+			x: props.arrow.x1 + PROTRUSION_LENGTH,
 			y: props.arrow.y1 + yDif * 0.25,
 		},
 		{ x: props.arrow.x1 + xDif * 0.5, y: props.arrow.y1 + yDif * 0.5 },
 		{
-			x: props.arrow.x2 - protrusionLength,
+			x: props.arrow.x2 - PROTRUSION_LENGTH,
 			y: props.arrow.y1 + yDif * 0.75,
 		},
-		{ x: props.arrow.x2 - protrusionLength, y: props.arrow.y2 },
+		{ x: props.arrow.x2 - PROTRUSION_LENGTH, y: props.arrow.y2 },
 		{ x: props.arrow.x2, y: props.arrow.y2 },
 	];
+	return ps;
+});
 
+const pathD = computed(() => {
+	const xDif = props.arrow.x2 - props.arrow.x1;
+	const ps = points.value;
 	let s: string;
 
-	if (xDif > protrusionLength) {
-		s = `M ${points[0].x} ${points[0].y}`;
-		s += `C ${points[3].x} ${points[0].y}, ${points[3].x} ${points[6].y}, ${points[6].x} ${points[6].y}`;
+	if (xDif > PROTRUSION_LENGTH) {
+		s = `M ${ps[0].x} ${ps[0].y}`;
+		s += `C ${ps[3].x} ${ps[0].y}, ${ps[3].x} ${ps[6].y}, ${ps[6].x} ${ps[6].y}`;
 	} else {
-		s = `M ${points[0].x} ${points[0].y}`;
-		s += `C ${points[1].x} ${points[1].y}, ${points[2].x} ${points[2].y}, ${points[3].x} ${points[3].y}`;
-		s += `C ${points[4].x} ${points[4].y}, ${points[5].x} ${points[5].y}, ${points[6].x} ${points[6].y}`;
+		s = `M ${ps[0].x} ${ps[0].y}`;
+		s += `C ${ps[1].x} ${ps[1].y}, ${ps[2].x} ${ps[2].y}, ${ps[3].x} ${ps[3].y}`;
+		s += `C ${ps[4].x} ${ps[4].y}, ${ps[5].x} ${ps[5].y}, ${ps[6].x} ${ps[6].y}`;
 	}
 
 	return s;
 });
+
+function handleDeleteClick() {
+	emit("delete");
+}
 </script>
 
 <style scoped>
 @import "@/renderer/sharedStyles.css";
-
-.WorkflowsArrow {
-}
 
 g {
 	outline: none;
@@ -80,5 +98,14 @@ g {
 
 path {
 	fill: none;
+}
+
+circle {
+	fill: white;
+	filter: drop-shadow(0 0 2px rgba(0, 0, 0, 0.2));
+}
+
+g.delete .cross {
+	stroke: black;
 }
 </style>
