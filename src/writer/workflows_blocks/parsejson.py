86ad41e63e0301539c180ccc72f9866aa53e23d6
Ramiro Medina <64783088+ramedina86@users.noreply.github.com>
2024-10-11 08:26:27 +0000

    feat: Parse JSON

---
+++ src/writer/workflows_blocks/parsejson.py
@@ -0,0 +1,48 @@
+import requests
+import json
+from writer.abstract import register_abstract_template
+from writer.ss_types import AbstractTemplate
+from writer.workflows_blocks.blocks import WorkflowBlock
+
+
+class ParseJSON(WorkflowBlock):
+
+    @classmethod
+    def register(cls, type: str):
+        super(ParseJSON, cls).register(type)
+        register_abstract_template(type, AbstractTemplate(
+            baseType="workflows_node",
+            writer={
+                "name": "Parse JSON",
+                "description": "Parses a JSON string.",
+                "category": "Content",
+                "fields": {
+                    "plainText": {
+                        "name": "Plain text",
+                        "type": "Text",
+                        "control": "Textarea"
+                    },
+                },
+                "outs": {
+                    "success": {
+                        "name": "Success",
+                        "description": "The request was successful.",
+                        "style": "success",
+                    },
+                    "error": {
+                        "name": "Error",
+                        "description": "The text provided couldn't be parsed.",
+                        "style": "error",
+                    },
+                },
+            }
+        ))
+
+    def run(self):
+        try:
+            plain_text = self._get_field("plainText")
+            self.result = json.loads(plain_text)
+            self.outcome = "success"
+        except BaseException as e:
+            self.outcome = "error"
+            raise e
