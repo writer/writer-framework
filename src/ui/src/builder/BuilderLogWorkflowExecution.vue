9097b86724d117e7a487869dd537cadf960c4188
Ramiro Medina <64783088+ramedina86@users.noreply.github.com>
2024-10-14 16:45:53 +0000

    chore: Added style to clickable cells

--- src/ui/src/builder/BuilderLogWorkflowExecution.vue
+++ src/ui/src/builder/BuilderLogWorkflowExecution.vue
@@ -7,7 +7,7 @@
 				<th>Time</th>
 			</tr>
 			<tr v-for="(entry, entryId) in enrichedExecutionLog" :key="entryId">
-				<td @click="selectBlock(entry.componentId)">
+				<td class="name" @click="selectBlock(entry.componentId)">
 					{{ entry.componentDef.name }}
 				</td>
 				<td>
@@ -78,5 +78,13 @@ td {
 	border: 1px solid var(--builderSeparatorColor);
 	margin: 0;
 	padding: 4px;
+}
+
+td.name {
+	cursor: pointer;
+}
+
+td.name:hover {
+	background: var(--builderSubtleSeparatorColor);
 }
 </style>
