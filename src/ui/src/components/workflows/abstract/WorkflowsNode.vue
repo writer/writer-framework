8bd9ba137934b659bcdbded6f4e825447868ceed
Ramiro Medina <64783088+ramedina86@users.noreply.github.com>
2024-10-26 09:39:35 +0000

    chore: Refactor, improved connection interaction

--- src/ui/src/components/workflows/abstract/WorkflowsNode.vue
+++ src/ui/src/components/workflows/abstract/WorkflowsNode.vue
@@ -21,8 +21,9 @@
 					:class="out.style"
 					:data-writer-socket-id="outId"
 					:data-writer-unselectable="true"
-					@click.capture="
-						(ev: DragEvent) => handleOutClick(ev, outId)
+					@click.capture.stop
+					@mousedown.capture="
+						(ev: DragEvent) => handleOutMousedown(ev, outId)
 					"
 				></div>
 			</div>
@@ -50,7 +51,7 @@ import { computed, inject, watch } from "vue";
 import { Component } from "@/writerTypes";
 import injectionKeys from "@/injectionKeys";
 
-const emit = defineEmits(["outSelect", "engaged"]);
+const emit = defineEmits(["outMousedown", "engaged"]);
 const wf = inject(injectionKeys.core);
 const wfbm = inject(injectionKeys.builderManager);
 const componentId = inject(injectionKeys.componentId);
@@ -92,9 +93,14 @@ const dynamicOuts = computed(() => {
 	return processedOuts;
 });
 
-function handleOutClick(ev: DragEvent, outId: string) {
+function handleOutMousedown(ev: DragEvent, outId: string) {
 	ev.stopPropagation();
-	emit("outSelect", outId);
+	emit("outMousedown", outId);
+}
+
+function handleOutMouseup(ev: DragEvent, outId: string) {
+	ev.stopPropagation();
+	emit("outMouseup", outId);
 }
 
 watch(isEngaged, () => {
@@ -110,6 +116,11 @@ watch(isEngaged, () => {
 	width: 240px;
 	position: absolute;
 	box-shadow: 0px 2px 0px 0px #f3f3f3;
+	user-select: none;
+}
+
+.WorkflowsNode:not(.selected) {
+	transition: border-color 0.2s ease-in-out;
 }
 
 .WorkflowsNode:hover {
