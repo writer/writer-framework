b2a5725aa3d187986dc88e915c274a8da43c9c80
Ramiro Medina <64783088+ramedina86@users.noreply.github.com>
2024-11-04 08:01:11 +0000

    chore: Standardize modifier key logic

--- src/ui/src/core/detectPlatform.ts
+++ src/ui/src/core/detectPlatform.ts
@@ -4,6 +4,14 @@ function getPlatform() {
 	return platform;
 }
 
+export function getModifierKeyName() {
+	return isPlatformMac() ? "⌘ Cmd" : "Ctrl";
+}
+
+export function isModifierKeyActive(ev: KeyboardEvent) {
+	return isPlatformMac() ? ev.metaKey : ev.ctrlKey;
+}
+
 export function isPlatformMac() {
 	const platform = getPlatform();
 	if (platform.toLowerCase().indexOf("mac") != -1) return true;
