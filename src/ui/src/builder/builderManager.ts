cccda430d5a4c88c53d057612467cdffd7383436
Ramiro Medina <64783088+ramedina86@users.noreply.github.com>
2024-11-04 08:04:19 +0000

    chore: Remove code mode, replace for panels

--- src/ui/src/builder/builderManager.ts
+++ src/ui/src/builder/builderManager.ts
@@ -1,9 +1,5 @@
 import { ref, Ref } from "vue";
-import {
-	Component,
-	ClipboardOperation,
-	WriterComponentDefinition,
-} from "@/writerTypes";
+import { Component, ClipboardOperation } from "@/writerTypes";
 
 export const CANDIDATE_CONFIRMATION_DELAY_MS = 1500;
 
@@ -22,8 +18,7 @@ export type WorkflowExecutionLog = {
 	componentId: Component["id"];
 	outcome: string;
 	executionTimeInSeconds: number;
-	component?: Component;
-	componentDef?: WriterComponentDefinition;
+	result: any;
 }[];
 
 type ComponentMutationTransaction = {
@@ -56,14 +51,13 @@ type LogEntry = {
 type SelectionSource = "click" | "tree" | "log";
 
 type State = {
-	mode: "ui" | "code" | "workflows" | "preview";
+	mode: "ui" | "workflows" | "preview";
 	openPanels: Set<"log" | "code">;
 	selection: {
 		componentId: Component["id"];
 		instancePath: string;
 		source: SelectionSource;
 	};
-	settingsBarCollapsed: boolean;
 	clipboard: {
 		operation: ClipboardOperation;
 		jsonSubtree: string;
@@ -79,7 +73,6 @@ export function generateBuilderManager() {
 	const initState: State = {
 		mode: "ui",
 		openPanels: new Set(),
-		settingsBarCollapsed: false,
 		selection: null,
 		clipboard: null,
 		mutationTransactionsSnapshot: {
@@ -145,14 +138,6 @@ export function generateBuilderManager() {
 
 	const getClipboard = () => {
 		return state.value.clipboard;
-	};
-
-	const setSettingsBarCollapsed = (newCollapsed: boolean) => {
-		state.value.settingsBarCollapsed = newCollapsed;
-	};
-
-	const isSettingsBarCollapsed = (): boolean => {
-		return state.value.settingsBarCollapsed;
 	};
 
 	const openMutationTransaction = (
@@ -317,8 +302,6 @@ export function generateBuilderManager() {
 		setMode,
 		getMode,
 		openPanels: state.value.openPanels,
-		setSettingsBarCollapsed,
-		isSettingsBarCollapsed,
 		isSelectionActive,
 		setSelection,
 		getSelection,
