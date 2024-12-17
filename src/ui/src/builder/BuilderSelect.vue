0af0a3c30e50f1464ec1f36057d4838313439c5b
Alexandre Rousseau <alexandre@rsseau.fr>
2024-12-17 10:57:53 +0100

    fix(ui): Set `BuilderSelect` WDS hover state

--- src/ui/src/builder/BuilderSelect.vue
+++ src/ui/src/builder/BuilderSelect.vue
@@ -131,6 +131,11 @@ function onSelect(value: string) {
 
 	cursor: pointer;
 }
+.BuilderSelect__trigger:focus {
+	border: 1px solid var(--softenedAccentColor);
+	box-shadow: 0px 0px 0px 3px rgba(81, 31, 255, 0.05);
+	outline: none;
+}
 .BuilderSelect__trigger__label {
 	text-overflow: ellipsis;
 	overflow: hidden;
