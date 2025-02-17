a0f525cd9b2f51d5e5419770014f489352eab036
Alexandre Rousseau <alexandre@rsseau.fr>
2025-02-17 11:33:06 +0100

    feat(ui): expand component tree on search and add animation - WF-183

--- src/ui/src/builder/BuilderTree.vue
+++ src/ui/src/builder/BuilderTree.vue
@@ -191,4 +191,26 @@ function toggleCollapse(newCollapse?: boolean) {
 		transition: unset;
 	}
 }
+
+.slide-fade-enter-active {
+	transition: all 0.1s ease-out;
+}
+
+.slide-fade-leave-active {
+	transition: all 0.1s ease-in;
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+	transform: translateY(-18px);
+	opacity: 0;
+}
+@media (prefers-reduced-motion) {
+	.slide-fade-enter-from,
+	.slide-fade-leave-to,
+	.slide-fade-enter-active,
+	.slide-fade-leave-active {
+		transition: unset;
+	}
+}
 </style>
