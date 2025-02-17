fe536c0f6c7f10b201589f2abd6442594d134765
Alexandre Rousseau <alexandre@rsseau.fr>
2025-02-17 14:32:03 +0100

    fix(ui): refresh ext. source file edit on code editor
    When user edits a source file with an external editor, we need to
    reflect this change in `sourceFileDraft` (monaco editor in the panel).

--- src/ui/src/core/useSourceFiles.ts
+++ src/ui/src/core/useSourceFiles.ts
@@ -4,7 +4,6 @@ import {
 	createFileToSourceFiles,
 	deleteFileToSourceFiles,
 	findSourceFileFromPath,
-	getSourceFilesPathsToEdges,
 	getSourceFilesPathsToFiles,
 	getSourceFilesPathsToNodes,
 	isSourceFilesFile,
@@ -19,6 +18,8 @@ export function useSourceFiles(wf: Core) {
 	watch(wf.sourceFiles, (currentSourceFiles, previousSourceFiles) => {
 		let wasUpdated = false;
 		let tree = structuredClone(toRaw(sourceFileDraft.value));
+		const pathsUnsavedStr =
+			getDifferentPaths(previousSourceFiles).map(pathToStr);
 
 		for (const path of getSourceFilesPathsToFiles(currentSourceFiles)) {
 			const prev = findSourceFileFromPath(path, previousSourceFiles);
@@ -38,10 +39,14 @@ export function useSourceFiles(wf: Core) {
 			}
 
 			if (!isSourceFilesFile(prev)) continue;
-			const wasLoaded = !prev.complete && cur.complete;
-			if (!wasLoaded) continue;
 
-			//  change because an element is loaded, we update the draft
+			const wasLoaded = !prev.complete && cur.complete;
+			const hasBeenChanged = prev.complete && cur.complete;
+			if (!wasLoaded && !hasBeenChanged) continue;
+
+			const pathStr = pathToStr(path);
+			if (pathsUnsavedStr.includes(pathStr)) continue;
+
 			const draft = findSourceFileFromPath(path, tree);
 			if (!isSourceFilesFile(draft)) continue;
 
@@ -63,25 +68,33 @@ export function useSourceFiles(wf: Core) {
 		if (wasUpdated) sourceFileDraft.value = tree;
 	});
 
+	function pathToStr(path: string[]) {
+		return path.join("/");
+	}
+
 	const filepathOpen = shallowRef<string[] | undefined>();
 
 	const sourceFilesDraftPaths = computed(() =>
 		Array.from(getSourceFilesPathsToFiles(sourceFileDraft.value)),
 	);
 
-	const pathsUnsaved = computed(() => {
+	const pathsUnsaved = computed(() =>
+		getDifferentPaths(wf.sourceFiles.value),
+	);
+
+	function getDifferentPaths(sourceFiles: SourceFiles) {
 		return sourceFilesDraftPaths.value.filter((path) => {
 			const draft = findSourceFileFromPath(path, sourceFileDraft.value);
 			if (!isSourceFilesFile(draft)) return true;
 
-			const file = findSourceFileFromPath(path, wf.sourceFiles.value);
+			const file = findSourceFileFromPath(path, sourceFiles);
 			if (!isSourceFilesFile(file)) return true;
 
 			if (!draft.complete || !file.complete) return false;
 
 			return draft.content !== file.content;
 		});
-	});
+	}
 
 	const fileOpen = computed(() => {
 		if (filepathOpen.value === undefined) return undefined;
