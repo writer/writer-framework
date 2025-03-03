d0c049298fb383bac4c51314b1a975aced778c7e
Alexandre Rousseau <alexandre@rsseau.fr>
2025-03-03 15:20:18 +0100

    feat(ui): implement list item in logs - WF-203

--- src/ui/src/builder/BuilderListItem.vue
+++ src/ui/src/builder/BuilderListItem.vue
@@ -1,25 +1,45 @@
 <script setup lang="ts">
 import { WdsColor } from "@/wds/tokens";
+import { computed } from "vue";
 
-defineProps({
+const props = defineProps({
 	isLast: { type: Boolean, required: false },
+	height: { type: Number, required: false, default: 1_000 },
 });
+
+const midHeight = computed(() => props.height / 2);
+const viewBox = computed(() => `0 0 17 ${props.height}`);
+
+const d1 = computed(() =>
+	[
+		"M1 0",
+		`V${midHeight.value - 4}`,
+		`C1 ${midHeight.value}.4183 4.58172 ${midHeight.value + 4} 9 ${midHeight.value + 4}`,
+		`H17`,
+	].join(" "),
+);
+
+const d2 = computed(() =>
+	[`M1 ${midHeight.value - 4}`, `V${props.height}`].join(" "),
+);
 </script>
 
 <template>
 	<div class="BuilderListItem">
 		<svg
-			viewBox="0 0 17 50"
+			:viewBox="viewBox"
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 			preserveAspectRatio="xMinYMax meet"
 			class="BuilderListItem__anchor"
 		>
+			<path :d="d1" :stroke="WdsColor.Blue2" stroke-width="2" />
 			<path
-				d="M1 0 V19 C1 23.4183 4.58172 27 9 27 H17"
+				v-if="!isLast"
+				:d="d2"
 				:stroke="WdsColor.Blue2"
+				stroke-width="2"
 			/>
-			<path v-if="!isLast" d="M1 19 V54" :stroke="WdsColor.Blue2" />
 		</svg>
 		<div class="BuilderListItem__content">
 			<slot />
@@ -45,6 +65,6 @@ defineProps({
 }
 .BuilderListItem__content {
 	width: 100%;
-	padding-left: calc(17px + 12px);
+	padding-left: 17px;
 }
 </style>
