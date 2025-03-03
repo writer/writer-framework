796b9cceac6249f777edc27e955202eccd1803d8
Alexandre Rousseau <alexandre@rsseau.fr>
2025-03-03 16:37:10 +0100

    fix(ui): improve dot animation - WF-203

--- src/ui/src/wds/WdsLoaderDots.vue
+++ src/ui/src/wds/WdsLoaderDots.vue
@@ -1,65 +1,42 @@
+<script setup lang="ts">
+import { WdsColor } from "./tokens";
+
+const color = WdsColor.Blue5;
+</script>
+
 <template>
-	<div class="WdsLoaderDots" :style="{ '--dotSize': '6px' }">
-		<span class="WdsLoaderDots__dot"></span>
-		<span class="WdsLoaderDots__dot"></span>
-		<span class="WdsLoaderDots__dot"></span>
-	</div>
+	<svg
+		width="16"
+		height="16"
+		viewBox="0 0 16 16"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<circle cx="4" cy="8" :fill="color">
+			<animate
+				attributeName="r"
+				values="0.5;3;0"
+				dur="1.2s"
+				repeatCount="indefinite"
+			/>
+		</circle>
+		<circle cx="8" cy="8" :fill="color">
+			<animate
+				attributeName="r"
+				values="0.5;3;0"
+				dur="1.2s"
+				begin="0.4s"
+				repeatCount="indefinite"
+			/>
+		</circle>
+		<circle cx="12" cy="8" :fill="color">
+			<animate
+				attributeName="r"
+				values="0.5;3;0"
+				dur="1.2s"
+				begin="0.8s"
+				repeatCount="indefinite"
+			/>
+		</circle>
+	</svg>
 </template>
-
-<style scoped>
-.WdsLoaderDots {
-	width: 100px;
-	height: 100%;
-	display: flex;
-	gap: var(--dotSize);
-	align-items: center;
-	justify-content: center;
-}
-
-.WdsLoaderDots__dot {
-	display: inline-block;
-	width: var(--dotSize);
-	height: var(--dotSize);
-	position: relative;
-}
-
-.WdsLoaderDots__dot::after {
-	display: inline-block;
-	content: "";
-	position: absolute;
-	background-color: currentColor;
-	width: calc(var(--dotSize) / 2);
-	height: calc(var(--dotSize) / 2);
-	border-radius: 50%;
-	animation: changeSize 1.5s infinite;
-
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-}
-
-.WdsLoaderDots__dot:nth-child(1)::after {
-	animation-delay: 0s;
-}
-.WdsLoaderDots__dot:nth-child(2)::after {
-	animation-delay: 0.5s;
-}
-.WdsLoaderDots__dot:nth-child(3)::after {
-	animation-delay: 1s;
-}
-
-@keyframes changeSize {
-	0% {
-		width: calc(var(--dotSize) / 2);
-		height: calc(var(--dotSize) / 2);
-	}
-	50% {
-		width: var(--dotSize);
-		height: var(--dotSize);
-	}
-	100% {
-		width: calc(var(--dotSize) / 2);
-		height: calc(var(--dotSize) / 2);
-	}
-}
-</style>
