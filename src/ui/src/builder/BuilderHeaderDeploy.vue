4e7e3331affe0913c279e25f618559895efd347e
Alexandre Rousseau <alexandre@rsseau.fr>
2025-03-27 14:41:38 +0100

    feat(ui): deploy app through iFrame - WF-210

--- src/ui/src/builder/BuilderHeaderDeploy.vue
+++ src/ui/src/builder/BuilderHeaderDeploy.vue
@@ -1,83 +1,40 @@
 <script setup lang="ts">
 import WdsButton from "@/wds/WdsButton.vue";
-import { computed, ref, watch } from "vue";
-import WdsFieldWrapper from "@/wds/WdsFieldWrapper.vue";
-import WdsTextInput from "@/wds/WdsTextInput.vue";
+import { inject, ref } from "vue";
 import WdsModal, { ModalAction } from "@/wds/WdsModal.vue";
+import {
+	useApplicationCloud,
+	DeploymentStatus,
+} from "@/composables/useApplicationCloud";
+import injectionKeys from "@/injectionKeys";
+const wf = inject(injectionKeys.core);
 
-const cancelModalAction: ModalAction = {
-	desc: "Cancel",
-	fn: cancelDeploy,
-};
+const { requestDeployment, deploymentStatus } = useApplicationCloud(wf);
 
 const confirmModalActions: ModalAction[] = [
-	cancelModalAction,
+	{ desc: "Cancel", fn: cancelDeploy },
 	{ desc: "Yes, deploy changes", fn: deploy },
 ];
 
-const apiKeyModalActions = computed<ModalAction[]>(() => [
-	cancelModalAction,
-	{ desc: "Deploy", fn: deploy, disabled: !hasApiKey.value },
-]);
-
 const confirmModalOpen = ref(false);
-const apiKeyModalOpen = ref(false);
-// fake status
-const isDeploying = ref(false);
-const isDirty = ref(false);
-const apiKey = ref("");
-const hasApiKey = computed(() => Boolean(apiKey.value));
-const lastDeployedAt = ref<Date | undefined>();
-
-const disabled = computed(() => !isDirty.value || isDeploying.value);
-
-function closeModals() {
-	confirmModalOpen.value = false;
-	apiKeyModalOpen.value = false;
-}
-
-function prepareDeploy() {
-	closeModals();
-
-	if (hasApiKey.value) {
-		confirmModalOpen.value = true;
-	} else {
-		apiKeyModalOpen.value = true;
-	}
-}
 
 function cancelDeploy() {
-	closeModals();
+	confirmModalOpen.value = false;
 }
 
-watch(
-	isDirty,
-	() => {
-		if (!isDirty.value) setTimeout(() => (isDirty.value = true), 2_000);
-	},
-	{ immediate: true },
-);
-
 function deploy() {
-	closeModals();
-	isDeploying.value = true;
-	setTimeout(() => {
-		lastDeployedAt.value = new Date();
-		isDeploying.value = false;
-		isDirty.value = false;
-	}, 3_000);
+	confirmModalOpen.value = false;
+	requestDeployment();
 }
 </script>
 
 <template>
 	<WdsButton
 		size="small"
-		:disabled="disabled"
-		:loading="isDeploying"
-		@click="prepareDeploy"
+		:loading="deploymentStatus === DeploymentStatus.InProgress"
+		@click="confirmModalOpen = true"
 	>
-		<template v-if="lastDeployedAt">Deploy changes</template>
-		<template v-else>Deploy</template>
+		Deploy
 		<WdsModal
 			v-if="confirmModalOpen"
 			title="Are you sure you want to deploy these changes?"
@@ -85,18 +42,6 @@ function deploy() {
 			size="normal"
 			:actions="confirmModalActions"
 		/>
-		<!-- API key modal -->
-		<WdsModal
-			v-if="apiKeyModalOpen"
-			title="Deploy app"
-			description="Add your API Key to deploy this agent"
-			size="normal"
-			:actions="apiKeyModalActions"
-		>
-			<WdsFieldWrapper label="API Key">
-				<WdsTextInput v-model="apiKey" />
-			</WdsFieldWrapper>
-		</WdsModal>
 	</WdsButton>
 </template>
 
--- src/ui/src/composables/useDebouncer.ts
+++
@@ -1,7 +0,0 @@
-export function useDebouncer(callback: () => void | Promise<void>, ms: number) {
-	let id: ReturnType<typeof setTimeout>;
-	return () => {
-		if (id) clearTimeout(id);
-		id = setTimeout(callback, ms);
-	};
-}
