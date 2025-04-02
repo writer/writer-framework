d64ff72b0df207f4d31d481b0da421a075053658
Alexandre Rousseau <alexandre@rsseau.fr>
2025-04-02 14:05:40 +0200

    feat(ui): implement new Key/Value editor - WF-233

---
+++ src/ui/src/builder/settings/BuilderObjectEditor.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { PropType } from "vue";
+
+type Mode = "assisted" | "freehand";
+defineProps({
+	mode: { type: String as PropType<Mode>, required: true },
+});
+
+defineModel({
+	type: Object as PropType<Record<string, string | number | boolean>>,
+});
+</script>
+
+<template>
+	<WdsTabs
+		class="BuilderFieldsOptions__tabs"
+		:tabs="tabs"
+		:model-value="mode"
+		@update:model-value="setMode"
+	/>
+</template>
