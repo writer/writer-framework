e2faa04c9f6eba403a10917e8d64b7626122a427
Alexandre Rousseau <alexandre@rsseau.fr>
2025-05-06 16:12:22 +0200

    feat(ui): implement shortcurts for new navbar - WF-375

--- src/ui/src/builder/sidebar/BuilderSidebar.vue
+++ src/ui/src/builder/sidebar/BuilderSidebar.vue
@@ -5,7 +5,7 @@
 				<BuilderSidebarButton
 					icon="stacks"
 					data-writer-tooltip-placement="right"
-					:data-writer-tooltip="paneTitles.layers"
+					:data-writer-tooltip="`${paneTitles.layers} (${modifierKeyName}I)`"
 					:active="activePane === 'layers'"
 					data-automation-action="sidebar-layers"
 					@click="changeActivePane('layers')"
@@ -13,7 +13,7 @@
 				<BuilderSidebarButton
 					icon="dashboard_customize"
 					data-writer-tooltip-placement="right"
-					:data-writer-tooltip="paneTitles.add"
+					:data-writer-tooltip="`${paneTitles.add} (${modifierKeyName}B)`"
 					:active="activePane === 'add'"
 					data-automation-action="sidebar-add"
 					@click="changeActivePane('add')"
@@ -25,22 +25,14 @@
 					icon="undo"
 					data-automation-key="undo"
 					data-writer-tooltip-placement="right"
-					:data-writer-tooltip="
-						undoRedoSnapshot.isUndoAvailable
-							? `Undo: ${undoRedoSnapshot.undoDesc}`
-							: 'Nothing to undo'
-					"
+					:data-writer-tooltip="`Undo (${modifierKeyName}Z)`"
 					:disabled="!undoRedoSnapshot.isUndoAvailable"
 					@click="undo()"
 				/>
 				<BuilderSidebarButton
 					data-automation-key="redo"
 					data-writer-tooltip-placement="right"
-					:data-writer-tooltip="
-						undoRedoSnapshot.isRedoAvailable
-							? `Redo: ${undoRedoSnapshot.redoDesc}`
-							: 'Nothing to redo'
-					"
+					:data-writer-tooltip="`Redo (${modifierKeyName}Y)`"
 					icon="redo"
 					:disabled="!undoRedoSnapshot.isRedoAvailable"
 					@click="redo()"
@@ -78,10 +70,19 @@
 import WdsButton from "@/wds/WdsButton.vue";
 import BuilderAsyncLoader from "../BuilderAsyncLoader.vue";
 import BuilderSidebarButton from "./BuilderSidebarButton.vue";
-import { computed, defineAsyncComponent, inject, ref, watch } from "vue";
+import {
+	computed,
+	defineAsyncComponent,
+	inject,
+	onMounted,
+	onUnmounted,
+	ref,
+	watch,
+} from "vue";
 import injectionKeys from "@/injectionKeys";
 import { useLocalStorageJSON } from "@/composables/useLocalStorageJSON";
 import { useComponentActions } from "../useComponentActions";
+import { getModifierKeyName, isPlatformMac } from "@/core/detectPlatform";
 
 const BuilderSidebarToolkit = defineAsyncComponent({
 	loader: () => import("./BuilderSidebarToolkit.vue"),
@@ -121,6 +122,35 @@ const paneTitles = computed<Record<Pane, string>>(() => ({
 	layers: wfbm.mode.value === "ui" ? "Interface Layers" : "Blueprint Layers",
 	add: "Add block",
 }));
+
+const modifierKeyName = getModifierKeyName();
+
+async function handleKeydown(ev: KeyboardEvent) {
+	const isModifierKeyActive = isPlatformMac() ? ev.metaKey : ev.ctrlKey;
+	if (!isModifierKeyActive) return;
+
+	const targetEl = ev.target as HTMLElement;
+	if (targetEl.closest("textarea, input, select")) return;
+
+	switch (ev.key) {
+		case "i":
+			ev.preventDefault();
+			activePane.value = "layers";
+			break;
+		case "b":
+			ev.preventDefault();
+			activePane.value = "add";
+			break;
+	}
+}
+const abort = new AbortController();
+
+onMounted(() => {
+	document.addEventListener("keydown", handleKeydown, {
+		signal: abort.signal,
+	});
+});
+onUnmounted(() => abort.abort());
 
 function changeActivePane(value: Pane) {
 	activePane.value = activePane.value === value ? undefined : value;
