eb8f55b50b62480ea8afc759cb5a2f55257241ae
Alexandre Rousseau <alexandre@rsseau.fr>
2025-05-23 14:08:33 +0200

    fix(ui): avoid run E2E tests for notes - WF-276

--- tests/e2e/tests/lowCode.spec.ts
+++ tests/e2e/tests/lowCode.spec.ts
@@ -2,25 +2,24 @@ import { test, expect } from "@playwright/test";
 import components from "writer-ui/components.codegen.json";
 
 const setInstruction = async (page, instruction) => {
-	await page.locator('div.TestInput textarea').click();
-	await page.locator('div.TestInput textarea').fill(instruction);
-}
+	await page.locator("div.TestInput textarea").click();
+	await page.locator("div.TestInput textarea").fill(instruction);
+};
 
 const execute = async (page) => {
-	await page.locator('button.TestExec').click();
-	await page.locator('button.TestExec').click();
-}
-
+	await page.locator("button.TestExec").click();
+	await page.locator("button.TestExec").click();
+};
 
 function generateValue(key, field) {
 	if (field.type === "Text") {
-		if(key === 'icon') {
-			return 'error-warning';
+		if (key === "icon") {
+			return "error-warning";
 		}
 		if (field.options) {
 			return Object.keys(field.options)[0];
 		}
-		
+
 		return "TEST_STRING";
 	} else if (field.type === "Number") {
 		return "7357";
@@ -36,7 +35,7 @@ function generateValue(key, field) {
 function generateValues(fields) {
 	const values = {};
 	Object.entries(fields).forEach(([key, field]) => {
-		const v =  generateValue(key, field);
+		const v = generateValue(key, field);
 		if (v !== null) {
 			values[key] = v;
 		}
@@ -44,110 +43,169 @@ function generateValues(fields) {
 	return values;
 }
 
-
 test.describe("low-code UI", () => {
 	let url: string;
-	test.beforeAll(async ({request}) => {
+	test.beforeAll(async ({ request }) => {
 		const response = await request.post(`/preset/low_code`);
 		expect(response.ok()).toBeTruthy();
-		({url} = await response.json());
+		({ url } = await response.json());
 	});
 
-	test.afterAll(async ({request}) => {
+	test.afterAll(async ({ request }) => {
 		await request.delete(url);
 	});
 
 	test.beforeEach(async ({ page }) => {
-		await page.goto(url, {waitUntil: "domcontentloaded"});
+		await page.goto(url, { waitUntil: "domcontentloaded" });
 	});
 
-	test("init_ui -  ui initialization" , async ({ page }) => {
-			await expect(page.locator(`.initialization .wf-type-text`)).toHaveText("Initialization successful!");
+	test("init_ui -  ui initialization", async ({ page }) => {
+		await expect(page.locator(`.initialization .wf-type-text`)).toHaveText(
+			"Initialization successful!",
+		);
 	});
 
-	components.forEach(({ type, name, fields, allowedParentTypes, toolkit }) => {
-		if (toolkit && toolkit !== "core") return;
-		const renderError = type === 'root' || (allowedParentTypes && !allowedParentTypes.includes('section'));
-		const componentName = name.replaceAll(/\s/g, "");
-		test(`create ${type} with ui.${componentName}() inside of section ${renderError ? '- render error' : ''}`, async ({ page }) => {
-			const props = generateValues(fields);
-			await setInstruction(page, `
+	components
+		.filter((c) => c.category !== "Internal")
+		.forEach(({ type, name, fields, allowedParentTypes, toolkit }) => {
+			if (toolkit && toolkit !== "core") return;
+			const renderError =
+				type === "root" ||
+				(allowedParentTypes && !allowedParentTypes.includes("section"));
+			const componentName = name.replaceAll(/\s/g, "");
+			test(`create ${type} with ui.${componentName}() inside of section ${renderError ? "- render error" : ""}`, async ({
+				page,
+			}) => {
+				const props = generateValues(fields);
+				await setInstruction(
+					page,
+					`
 with ui.find('results'):
 		ui.${componentName}(${JSON.stringify(props)})
-			`);
-			await execute(page);
-			await expect(page.locator(`.results .wf-type-${type}.component`)).toHaveCount(1);
+			`,
+				);
+				await execute(page);
+				await expect(
+					page.locator(`.results .wf-type-${type}.component`),
+				).toHaveCount(1);
 
-			await page.locator(`.results .wf-type-${type}.component`).click({force: true});
-			for (const [key, value] of Object.entries(props)) {
-				await expect(page.locator(`.BuilderSettings div[data-automation-key="${key}"] input, .BuilderSettings div[data-automation-key="${key}"] textarea`)).toHaveCount(1);
-				await expect(page.locator(`.BuilderSettings div[data-automation-key="${key}"] input, .BuilderSettings div[data-automation-key="${key}"] textarea`)).toHaveValue(value);
-			}
+				await page
+					.locator(`.results .wf-type-${type}.component`)
+					.click({ force: true });
+				for (const [key, value] of Object.entries(props)) {
+					await expect(
+						page.locator(
+							`.BuilderSettings div[data-automation-key="${key}"] input, .BuilderSettings div[data-automation-key="${key}"] textarea`,
+						),
+					).toHaveCount(1);
+					await expect(
+						page.locator(
+							`.BuilderSettings div[data-automation-key="${key}"] input, .BuilderSettings div[data-automation-key="${key}"] textarea`,
+						),
+					).toHaveValue(value);
+				}
 
-			if (renderError) {
-				await expect(page.locator(`.results .wf-type-${type}.component`)).toHaveClass(/RenderError/);
-			}else{
-				await expect(page.locator(`.results .wf-type-${type}.component`)).not.toHaveClass(/RenderError/);
-			}
+				if (renderError) {
+					await expect(
+						page.locator(`.results .wf-type-${type}.component`),
+					).toHaveClass(/RenderError/);
+				} else {
+					await expect(
+						page.locator(`.results .wf-type-${type}.component`),
+					).not.toHaveClass(/RenderError/);
+				}
+			});
 		});
-	});
 	test("settings should be enabled for bmc", async ({ page }) => {
-			await page.locator(`.results`).click({force: true});
-			await expect(page.locator(`.BuilderSettingsMain > .sections:not(.debug)`)).not.toHaveAttribute("inert");
-			await expect(page.locator(`.BuilderSettingsMain > .cmc-warning`)).toHaveCount(0);
+		await page.locator(`.results`).click({ force: true });
+		await expect(
+			page.locator(`.BuilderSettingsMain > .sections:not(.debug)`),
+		).not.toHaveAttribute("inert");
+		await expect(
+			page.locator(`.BuilderSettingsMain > .cmc-warning`),
+		).toHaveCount(0);
 	});
 
 	test("settings should be disabled for cmc", async ({ page }) => {
-			await setInstruction(page, `
+		await setInstruction(
+			page,
+			`
 with ui.find('results'):
 			ui.Text({"cssClasses": "out", "text": "Hello"})
-			`);
-			await execute(page);
-			await page.locator(`.results .wf-type-text.component.out`).click({force: true});
-			await expect(page.locator(`.BuilderSettingsMain > .sections:not(.debug)`)).toHaveAttribute("inert");
-			await expect(page.locator(`.BuilderSettingsMain > .cmc-warning`)).toHaveCount(1);
+			`,
+		);
+		await execute(page);
+		await page
+			.locator(`.results .wf-type-text.component.out`)
+			.click({ force: true });
+		await expect(
+			page.locator(`.BuilderSettingsMain > .sections:not(.debug)`),
+		).toHaveAttribute("inert");
+		await expect(
+			page.locator(`.BuilderSettingsMain > .cmc-warning`),
+		).toHaveCount(1);
 	});
 
 	test("create nested components", async ({ page }) => {
-			await setInstruction(page, `
+		await setInstruction(
+			page,
+			`
 with ui.find('results'):
 	with ui.ColumnContainer({"cssClasses": "level1"}):
 		with ui.Column({"cssClasses": "level2"}):
 			ui.Text({"cssClasses": "level3", "text": "Hello"})
-			`);
-			await execute(page);
-			await expect(page.locator(`.results .wf-type-columns.level1 .wf-type-column.level2 .wf-type-text.level3`)).toHaveCount(1);
-			await expect(page.locator(`.results .wf-type-columns.level1 .wf-type-column.level2 .wf-type-text.level3`)).toHaveText("Hello");
+			`,
+		);
+		await execute(page);
+		await expect(
+			page.locator(
+				`.results .wf-type-columns.level1 .wf-type-column.level2 .wf-type-text.level3`,
+			),
+		).toHaveCount(1);
+		await expect(
+			page.locator(
+				`.results .wf-type-columns.level1 .wf-type-column.level2 .wf-type-text.level3`,
+			),
+		).toHaveText("Hello");
 	});
 
 	test("binding state", async ({ page }) => {
-			await setInstruction(page, `
+		await setInstruction(
+			page,
+			`
 with ui.find('results'):
 	ui.TextInput({"cssClasses": "in"}, binding={"wf-change": "value"})
 	ui.Text({"cssClasses": "out", "text": "@{value}"})
-			`);
-			await execute(page);
-			await page.locator(`.results .in input`).fill("Hello");
-			await expect(page.locator(`.results .out`)).toHaveText("Hello");
+			`,
+		);
+		await execute(page);
+		await page.locator(`.results .in input`).fill("Hello");
+		await expect(page.locator(`.results .out`)).toHaveText("Hello");
 	});
 
 	test("visibility", async ({ page }) => {
-			await setInstruction(page, `
+		await setInstruction(
+			page,
+			`
 with ui.find('results'):
 	ui.Text({"cssClasses": "out", "text": "Hello"}, visible="no")
-			`);
-			await execute(page);
-			await expect(page.locator(`.results .out`)).toBeHidden();
+			`,
+		);
+		await execute(page);
+		await expect(page.locator(`.results .out`)).toBeHidden();
 	});
 
 	test("handlers", async ({ page }) => {
-			await setInstruction(page, `
+		await setInstruction(
+			page,
+			`
 with ui.find('results'):
 	ui.TextInput({"cssClasses": "in"}, handlers={"wf-change": update_value})
 	ui.Text({"cssClasses": "out", "text": "@{value}"})
-			`);
-			await execute(page);
-			await page.locator(`.results .in input`).fill("Hello");
-			await expect(page.locator(`.results .out`)).toHaveText("Hello");
+			`,
+		);
+		await execute(page);
+		await page.locator(`.results .in input`).fill("Hello");
+		await expect(page.locator(`.results .out`)).toHaveText("Hello");
 	});
 });
