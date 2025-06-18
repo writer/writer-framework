d7234f1717182ec5daeab8cac8464370b2bcacf5
UladzislauK-Writer <uladzislau.kasabutski@writer.com>
2025-06-18 13:23:17 +0200

    feat: new autogen design

--- src/ui/src/components/blueprints/BlueprintsAutogen.vue
+++ src/ui/src/components/blueprints/BlueprintsAutogen.vue
@@ -1,25 +1,46 @@
 <template>
 	<div class="BlueprintsAutogen">
-		<template v-if="!isBusy">
-			<div class="main">
-				<WdsTextareaInput
-					v-model="prompt"
-					rows="8"
-					placeholder="Describe the blocks you'd like to generate..."
-				></WdsTextareaInput>
-			</div>
-			<div class="buttons">
-				<WdsButton @click="handleAutogen">
-					<i class="material-symbols-outlined">bolt</i> Generate
-				</WdsButton>
-				<WdsButton variant="tertiary" @click="handleCancel">
-					Cancel
-				</WdsButton>
-			</div>
-		</template>
+		<h2>
+			{{
+				isBusy
+					? "Generating agent"
+					: "What would you like this agent to do?"
+			}}
+		</h2>
+		<BlueprintsAutogenContents :is-busy="isBusy" :class="{ busy: isBusy }">
+			<template v-if="!isBusy">
+				<div
+					class="BlueprintsAutogen__main"
+					:class="{ error: errorMessage }"
+				>
+					<WdsTextareaInput
+						v-model="prompt"
+						rows="10"
+						placeholder="Describe the blocks you'd like to generate..."
+						style="height: 100%"
+					></WdsTextareaInput>
+				</div>
+			</template>
 
-		<template v-else>
-			<BlueprintsGenerationLoader></BlueprintsGenerationLoader>
+			<template v-else>
+				<BlueprintsGenerationLoader />
+			</template>
+		</BlueprintsAutogenContents>
+		<template v-if="!isBusy">
+			<div class="BlueprintsAutogen__panel">
+				<p class="BlueprintsAutogen__error">
+					{{ errorMessage }}
+				</p>
+				<div class="BlueprintsAutogen__buttons">
+					<WdsButton variant="tertiary" @click="handleCancel">
+						Skip
+					</WdsButton>
+					<WdsButton variant="secondary" @click="handleAutogen">
+						<i class="material-symbols-outlined">bolt</i>
+						Autogenerate agent
+					</WdsButton>
+				</div>
+			</div>
 		</template>
 	</div>
 </template>
@@ -29,6 +50,7 @@ import { inject, onMounted, ref } from "vue";
 import WdsButton from "@/wds/WdsButton.vue";
 import WdsTextareaInput from "@/wds/WdsTextareaInput.vue";
 
+import BlueprintsAutogenContents from "./BlueprintsAutogenContents.vue";
 import BlueprintsGenerationLoader from "./BlueprintsGenerationLoader.vue";
 import { Component } from "@/writerTypes";
 import { useComponentActions } from "@/builder/useComponentActions";
@@ -44,6 +66,7 @@ const tracking = useWriterTracking(wf);
 
 const isBusy = ref(false);
 const prompt = ref("");
+const errorMessage = ref<string | null>(null); // Track error messages
 
 const emits = defineEmits(["blockGeneration"]);
 
@@ -101,25 +124,34 @@ function alterIds(components: Component[]) {
 async function handleAutogen() {
 	const description = prompt.value;
 	isBusy.value = true;
+	errorMessage.value = null;
 	tracking.track("blueprints_auto_gen_started", { prompt: prompt.value });
-	const response = await fetch(convertAbsolutePathtoFullURL("/api/autogen"), {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({ description }),
-	});
 
-	isBusy.value = false;
+	try {
+		const response = await fetch(
+			convertAbsolutePathtoFullURL("/api/autogen"),
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ description }),
+			},
+		);
+		if (!response.ok) {
+			errorMessage.value = `Agent failed to generate. Try again.`;
+			return;
+		}
 
-	if (!response.ok) {
-		throw new Error(`Error: ${response.status} - ${response.statusText}`);
+		const data = await response.json(); // Assuming the response is JSON
+		const components: Component[] = alterIds(data.blueprint?.components);
+		emits("blockGeneration", { components });
+	} catch {
+		errorMessage.value = `Agent failed to generate. Try again.`;
+		return;
+	} finally {
+		isBusy.value = false;
 	}
-
-	const data = await response.json(); // Assuming the response is JSON
-
-	const components: Component[] = alterIds(data.blueprint?.components);
-	emits("blockGeneration", { components });
 
 	tracking.track("blueprints_auto_gen_completed");
 }
@@ -128,13 +160,20 @@ async function handleAutogen() {
 <style scoped>
 .BlueprintsAutogen {
 	display: flex;
-	gap: 24px;
+	gap: 20px;
 	flex-direction: column;
-	align-items: center;
 }
 
-.main {
+.BlueprintsAutogen__main {
 	width: 100%;
+	padding: 0;
+	margin: 0;
+}
+
+.BlueprintsAutogen__main.error {
+	border: 1px solid var(--wdsColorOrange5);
+	border-radius: 8px;
+	box-sizing: border-box;
 }
 
 h2 {
@@ -143,10 +182,28 @@ h2 {
 	font-style: normal;
 	font-weight: 500;
 	line-height: 160%;
+	text-align: left;
 }
 
-.buttons {
+.BlueprintsAutogen__panel {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+
+.BlueprintsAutogen__error {
+	display: flex;
+	color: var(--wdsColorOrange5);
+	font-size: 12px;
+	font-weight: 400;
+	line-height: 180%;
+	align-items: flex-start;
+}
+
+.BlueprintsAutogen__buttons {
 	display: flex;
 	gap: 16px;
+	justify-content: flex-end;
+	padding-right: 20px;
 }
 </style>
