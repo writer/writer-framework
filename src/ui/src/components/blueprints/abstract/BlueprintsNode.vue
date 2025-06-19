1d6ca3221c9c5fe6c923540969fcccef3c718d54
Mateusz Russak <mateusz@russak.biz>
2025-06-19 13:13:48 +0200

    feat: node status based on severity in blueprint

--- src/ui/src/components/blueprints/abstract/BlueprintsNode.vue
+++ src/ui/src/components/blueprints/abstract/BlueprintsNode.vue
@@ -145,21 +145,44 @@ const completionStyle = computed(() => {
 	);
 });
 
-const latestKnownOutcome = computed(() => {
+const latestRun = computed(() => {
 	const logEntries = wfbm.getLogEntries();
+	const runId = logEntries.find((entry) => {
+		return !!entry.blueprintExecution;
+	})?.blueprintExecution?.runId ?? null;
+	return logEntries.filter((entry) => {
+		return entry?.blueprintExecution?.runId === runId;
+	});
+});
 
-	for (let i = 0; i < logEntries.length; i++) {
-		const logEntry = logEntries[i];
-		const we = logEntry.blueprintExecution;
-		if (!we) continue;
-		for (let j = 0; j < we.summary.length; j++) {
-			const item = we.summary[j];
-			if (item.componentId !== component.value.id) continue;
-			return item.outcome;
-		}
+const outcomeSeverity = {
+	in_progress: 5,
+	error: 4,
+	success: 3,
+	cancelled: 2,
+	skipped: 1,
+	none: 0,
+}
+
+const latestKnownOutcome = computed(() => {
+	const executionLogs = latestRun.value.map((entry) => {
+		return entry.blueprintExecution;
+	}).filter(Boolean);
+	let outcome = 'none';
+	executionLogs.forEach((log) => {
+		log.summary.forEach((item) => {
+			if (item.outcome && item.componentId === component.value.id) {
+				const severity = (!Object.keys(outcomeSeverity).includes(item.outcome) ? outcomeSeverity.success : outcomeSeverity[item.outcome]);
+				if (severity > outcomeSeverity[outcome]) {
+					outcome = item.outcome;
+				}
+			}
+		});
+	});
+	if (outcome === 'none') {
+		return null;
 	}
-
-	return null;
+	return outcome;
 });
 
 const isEngaged = computed(() => {
