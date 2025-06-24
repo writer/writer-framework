946167ea3d4f38bc57abc95641d9f505868dc003
Alexandre Rousseau <alexandre@rsseau.fr>
2025-06-24 11:04:42 +0200

    Revert "feat(ui): allow copying multi-select in blueprint - WF-502"
    This reverts commit 171d18324302490ecf342fbe972f1888fac9aad8.

--- src/ui/src/builder/settings/BuilderSettingsActions.vue
+++ src/ui/src/builder/settings/BuilderSettingsActions.vue
@@ -13,21 +13,6 @@
 			<i class="material-symbols-outlined">close</i>
 		</WdsButton>
 		<WdsButton
-			v-if="
-				ssbm.selectionStatus.value === SelectionStatus.Multiple &&
-				ssbm.mode.value === 'blueprints'
-			"
-			class="actionButton"
-			variant="neutral"
-			size="small"
-			:data-writer-tooltip="`Copy (${getModifierKeyName()}C)`"
-			data-writer-tooltip-placement="left"
-			:disabled="isCopyDisabled"
-			@click="copySelectedComponents"
-		>
-			<i class="material-symbols-outlined">content_copy</i>
-		</WdsButton>
-		<WdsButton
 			class="BuilderSettingsActions__btn BuilderSettingsActions__btn--delete"
 			variant="neutral"
 			size="small"
@@ -45,7 +30,6 @@
 <script setup lang="ts">
 import { computed, inject } from "vue";
 import injectionKeys from "@/injectionKeys";
-import { getModifierKeyName } from "@/core/detectPlatform";
 import WdsButton from "@/wds/WdsButton.vue";
 import { SelectionStatus } from "../builderManager";
 import { useWriterTracking } from "@/composables/useWriterTracking";
@@ -68,20 +52,10 @@ const { dropdownOptions, handleDropdownSelect } = useBuilderSettingsActions(
 function deleteSelectedComponents() {
 	handleDropdownSelect(BuilderSettingsDropdownActions.Delete);
 }
-function copySelectedComponents() {
-	handleDropdownSelect(BuilderSettingsDropdownActions.Copy);
-}
 
 const isDeleteDisabled = computed(() => {
 	const option = dropdownOptions.value.find(
 		(o) => o.value === BuilderSettingsDropdownActions.Delete,
-	);
-	if (!option) return true;
-	return option.disabled;
-});
-const isCopyDisabled = computed(() => {
-	const option = dropdownOptions.value.find(
-		(o) => o.value === BuilderSettingsDropdownActions.Copy,
 	);
 	if (!option) return true;
 	return option.disabled;
