70eb78504a15ae2a54eab9c46936c686fca6d340
Colin McNeil <colin.mcneil@writer.com>
2025-07-01 13:15:47 -0400

    Dropdwon UX Improvements

--- src/ui/src/builder/BuilderApp.vue
+++ src/ui/src/builder/BuilderApp.vue
@@ -273,7 +273,7 @@ async function handleKeydown(ev: KeyboardEvent) {
 	const { componentId: selectedId, instancePath: selectedInstancePath } =
 		ssbm.firstSelectedItem.value;
 
-	if (ev.key == "Delete") {
+	if (ev.key == (isPlatformMac() ? "Backspace" : "Delete")) {
 		const componentIds = ssbm.selection.value
 			.filter((s) => isDeleteAllowed(s.componentId))
 			.map((s) => s.componentId);
