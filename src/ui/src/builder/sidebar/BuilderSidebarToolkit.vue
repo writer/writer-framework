ae5128c651548a453b38d54e53cab4466032ad09
Vlad Krasny <157218536+VladKrasny@users.noreply.github.com>
2025-07-03 14:59:22 +0000

    feat(ui): order all blocks in Interface and Blueprint to be alphabetical per section - AB-101

--- src/ui/src/builder/sidebar/BuilderSidebarToolkit.vue
+++ src/ui/src/builder/sidebar/BuilderSidebarToolkit.vue
@@ -139,9 +139,12 @@ function getRelevantToolsInCategory(categoryId: string) {
 		return { type, name, description, category: category ?? "Other" };
 	});
 	const q = query.value.toLocaleLowerCase();
-	const queryApplied = enriched.filter(
-		(tool) => !q || tool.name.toLocaleLowerCase().includes(q),
-	);
+	const queryApplied = enriched
+		.filter((tool) => !q || tool.name.toLocaleLowerCase().includes(q))
+		.sort((a, b) =>
+			a.name.localeCompare(b.name, undefined, { sensitivity: "base" }),
+		);
+
 	return queryApplied;
 }
 
