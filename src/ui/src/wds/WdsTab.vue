c52e63e5bdda2c3f7bbebc39bee7ce99b577eb8e
Vlad Krasny <157218536+VladKrasny@users.noreply.github.com>
2025-07-25 14:04:21 +0000

    feat(WdsTab, WdsTabs): add variant support for tabs

--- src/ui/src/wds/WdsTab.vue
+++ src/ui/src/wds/WdsTab.vue
@@ -1,22 +1,36 @@
 <template>
 	<button
 		class="WdsTab"
-		:class="{ 'WdsTab--disabled': disabled, 'WdsTab--selected': selected }"
+		:class="{
+			'WdsTab--disabled': disabled,
+			'WdsTab--selected': selected,
+			'WdsTab--variant-bar': variant === 'bar',
+			'WdsTab--variant-chip': variant === 'chip',
+		}"
 		type="button"
 		:disabled="Boolean(disabled)"
 		:data-writer-tooltip="tooltip"
 		@click="$emit('click')"
 	>
 		<slot />
+		<div v-if="variant === 'bar'" class="WdsTab__indicator"></div>
 	</button>
 </template>
 
+<script lang="ts">
+export type WdsTabVariant = "chip" | "bar";
+</script>
+
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, PropType } from "vue";
 
 const props = defineProps({
 	disabled: { type: [Boolean, String], default: undefined },
 	selected: { type: Boolean },
+	variant: {
+		type: String as PropType<WdsTabVariant>,
+		required: true,
+	},
 });
 
 const tooltip = computed(() =>
@@ -31,25 +45,57 @@ defineEmits({
 <style scoped>
 .WdsTab {
 	/* reset button */
+	position: relative;
 	border: none;
 	background-color: transparent;
 	cursor: pointer;
-
-	padding: 4px 20px 4px 20px;
-	gap: 10px;
-	border-radius: 8px;
 }
 
-.WdsTab:hover {
-	background-color: var(--wdsColorBlue1);
+.WdsTab__indicator {
+	pointer-events: none;
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	height: 2px;
+	width: 100%;
+	background: currentColor;
+	opacity: 0;
 }
 
 .WdsTab--disabled {
-	color: var(--wdsColorGray4);
 	cursor: not-allowed;
 }
 
-.WdsTab--selected {
+.WdsTab--variant-bar {
+	padding: 10px 0;
+	font-size: 14px;
+	line-height: 180%;
+	color: var(--wdsColorGray5);
+}
+
+.WdsTab--variant-bar.WdsTab--selected {
+	color: var(--wdsColorBlack);
+}
+
+.WdsTab--variant-bar.WdsTab--selected .WdsTab__indicator {
+	opacity: 1;
+}
+
+.WdsTab--variant-chip {
+	padding: 4px 20px 4px 20px;
+	border-radius: 8px;
+}
+
+.WdsTab--variant-chip:hover {
+	background-color: var(--wdsColorBlue1);
+}
+
+.WdsTab--variant-chip.WdsTab--selected {
 	background-color: var(--wdsColorBlue2);
+}
+
+.WdsTab--variant-chip.WdsTab--disabled {
+	color: var(--wdsColorGray4);
 }
 </style>
--- src/ui/src/wds/WdsTabs.vue
+++ src/ui/src/wds/WdsTabs.vue
@@ -1,8 +1,15 @@
 <template>
-	<div class="WdsTabs">
+	<div
+		class="WdsTabs"
+		:class="{
+			'WdsTabs--variant-bar': variant === 'bar',
+			'WdsTabs--variant-chip': variant === 'chip',
+		}"
+	>
 		<WdsTab
 			v-for="tab of tabs"
 			:key="tab.value"
+			:variant
 			:disabled="tab.disabled"
 			:selected="tab.value === selected"
 			@click="selected = tab.value"
@@ -21,12 +28,16 @@ export interface WdsTabOptions<Value extends string = string> {
 
 <script setup lang="ts">
 import { PropType } from "vue";
-import WdsTab from "./WdsTab.vue";
+import WdsTab, { type WdsTabVariant } from "./WdsTab.vue";
 
 defineProps({
 	tabs: {
 		type: Array as PropType<WdsTabOptions[] | Readonly<WdsTabOptions[]>>,
 		required: true,
+	},
+	variant: {
+		type: String as PropType<WdsTabVariant>,
+		default: "chip",
 	},
 });
 
@@ -36,6 +47,15 @@ const selected = defineModel({ type: String });
 <style scoped>
 .WdsTabs {
 	display: flex;
+}
+
+.WdsTabs--variant-bar {
+	gap: 32px;
+	overflow-x: auto;
+	border-bottom: 1px solid var(--wdsColorGray2);
+}
+
+.WdsTabs--variant-chip {
 	gap: 16px;
 	flex-wrap: wrap;
 }
