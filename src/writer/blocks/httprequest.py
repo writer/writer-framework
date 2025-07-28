5a089e645c5ffc5a5db4abf0dc2b84d58d5158ad
Alexandre Rousseau <alexandre@rsseau.fr>
2025-07-28 10:11:57 +0200

    fix: remove URL validation - AB-64

--- src/writer/blocks/httprequest.py
+++ src/writer/blocks/httprequest.py
@@ -38,10 +38,6 @@ class HTTPRequest(BlueprintBlock):
                             "name": "URL",
                             "type": "Text",
                             "control": "Textarea",
-                            "validator": {
-                                "type": "string",
-                                "format": "uri",
-                            },
                         },
                         "headers": {
                             "name": "Headers",
@@ -94,9 +90,7 @@ class HTTPRequest(BlueprintBlock):
         try:
             method = self._get_field("method", False, "GET")
             url = self._get_field("url")
-            headers = self._get_field(
-                "headers", True, default_field_value="{}"
-                )
+            headers = self._get_field("headers", True, default_field_value="{}")
             body_type = self._get_field("bodyType")
             body = None
             raw_body = None
@@ -110,13 +104,7 @@ class HTTPRequest(BlueprintBlock):
                 raw_body = body
 
             with self.acquire_httpx_client() as client:
-                res = client.request(
-                    method,
-                    url,
-                    headers=headers,
-                    content=raw_body,
-                    timeout=180
-                    )
+                res = client.request(method, url, headers=headers, content=raw_body, timeout=180)
 
                 content_type = res.headers.get("Content-Type", "")
                 is_response_json = "application/json" in content_type
@@ -125,9 +113,7 @@ class HTTPRequest(BlueprintBlock):
                     "request": {
                         "url": str(res.request.url),
                         "headers": dict(res.request.headers),
-                        "body": res.request.content.decode(
-                            "utf-8", errors="replace"
-                            ),
+                        "body": res.request.content.decode("utf-8", errors="replace"),
                     },
                     "headers": dict(res.headers),
                     "status_code": res.status_code,
@@ -139,9 +125,8 @@ class HTTPRequest(BlueprintBlock):
                 else:
                     self.outcome = "responseError"
                     raise RuntimeError(
-                        f"HTTP response with code {res.status_code}"
-                        f" and message {res.text}"
-                        )
+                        f"HTTP response with code {res.status_code} and message {res.text}"
+                    )
 
         except json.JSONDecodeError as e:
             self.outcome = "responseError"
