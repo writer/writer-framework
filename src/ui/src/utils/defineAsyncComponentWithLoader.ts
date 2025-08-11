86fbce2aba9e6e7d96dad218f6142b9d11794366
Alexandre Rousseau <alexandre@rsseau.fr>
2025-08-11 10:13:59 +0200

    fix(ui): improve comp loading error UI

--- src/ui/src/utils/defineAsyncComponentWithLoader.ts
+++ src/ui/src/utils/defineAsyncComponentWithLoader.ts
@@ -1,25 +1,53 @@
 import WdsSkeletonLoader from "@/wds/WdsSkeletonLoader.vue";
-import { defineComponent, h } from "vue";
+import { h } from "vue";
 import { AsyncComponentOptions, defineAsyncComponent } from "vue";
 
-const ErrorComponent = defineComponent({
-	render: () =>
+function errorComponent() {
+	return h(
+		"div",
+		{
+			style: {
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+			},
+		},
 		h(
 			"p",
 			{
 				style: {
 					background: "var(--wdsColorOrange2)",
 					padding: "8px",
+					borderRadius: "4px",
 				},
 			},
 			"Could not load the component",
 		),
-});
+	);
+}
+
+function loadingComponent() {
+	return h(
+		"div",
+		{
+			style: {
+				display: "flex",
+				alignItems: "center",
+				justifyContent: "center",
+				height: "100%",
+				width: "100%",
+				padding: "8px",
+			},
+		},
+		h(WdsSkeletonLoader),
+	);
+}
 
 export function defineAsyncComponentWithLoader(options: AsyncComponentOptions) {
 	return defineAsyncComponent({
-		loadingComponent: WdsSkeletonLoader,
-		errorComponent: ErrorComponent,
+		loadingComponent,
+		errorComponent,
+		delay: 300,
 		...options,
 	});
 }
