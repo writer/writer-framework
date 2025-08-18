217e193b4cd1c268dbfcf3e8355e6cb4680e2f32
Alexandre Rousseau <alexandre@rsseau.fr>
2025-08-18 19:50:26 +0200

    Revert "fix(writer): filter notes when running a blueprint - AB-234"

--- src/writer/blueprints.py
+++ src/writer/blueprints.py
@@ -226,8 +226,7 @@ class BlueprintRunner:
             if not node:
                 break
             if node.type == "blueprints_blueprint":
-                nodes = self.session.session_component_tree.get_descendents(current_node_id)
-                return [node for node in nodes if node.type != 'note']
+                return self.session.session_component_tree.get_descendents(current_node_id)
             current_node_id = node.parentId
         return []
 
