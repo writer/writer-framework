1811bce2de4436d0061a89ba1f6287ecc1fd3ca5
Vlad Krasny <157218536+VladKrasny@users.noreply.github.com>
2025-08-25 11:11:54 +0000

    feat: add attr-accept package and simplify file type acceptance logic - AB-27

--- src/ui/src/composables/useFileTypeAccept.ts
+++ src/ui/src/composables/useFileTypeAccept.ts
@@ -1,4 +1,5 @@
 import { computed, DeepReadonly, MaybeRef, toValue } from "vue";
+import accept from "attr-accept";
 
 export type UseFileTypeAcceptParams = {
 	acceptedFileTypes?: DeepReadonly<MaybeRef<string[]>>;
@@ -35,46 +36,8 @@ export function useFileTypeAccept({
 		() => normalizedAcceptedFileTypes.value.join(",") || undefined,
 	);
 
-	const matchers = computed(() => {
-		const extensions = new Set<string>();
-		const wildcards = new Set<string>();
-		const mimes = new Set<string>();
-
-		normalizedAcceptedFileTypes.value.forEach((fileType) => {
-			if (fileType.startsWith(".")) {
-				extensions.add(fileType);
-			} else if (fileType.endsWith("/*")) {
-				// 'image/*' -> 'image/'
-				wildcards.add(fileType.slice(0, -1));
-			} else {
-				mimes.add(fileType);
-			}
-		});
-
-		return {
-			extensions,
-			wildcards: [...wildcards],
-			mimes,
-		};
-	});
-
 	function checkIsFileAccepted(file: File): boolean {
-		if (normalizedAcceptedFileTypes.value.length === 0) {
-			// no restrictions, all files are accepted
-			return true;
-		}
-
-		const fileType = (file.type || "").toLowerCase();
-		const fileName = (file.name || "").toLowerCase();
-
-		const dotIndex = fileName.lastIndexOf(".");
-		const fileExtension = dotIndex > -1 ? fileName.slice(dotIndex) : "";
-
-		return (
-			matchers.value.mimes.has(fileType) ||
-			matchers.value.wildcards.some((p) => fileType.startsWith(p)) ||
-			matchers.value.extensions.has(fileExtension)
-		);
+		return accept(file, normalizedAcceptedFileTypes.value);
 	}
 
 	return {
