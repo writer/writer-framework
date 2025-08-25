631a5f8853a2a2d9a2ba2cd4feeec4c0ea54cd3f
Vlad Krasny <157218536+VladKrasny@users.noreply.github.com>
2025-08-25 10:44:04 +0000

    ui(SharedFile): change tooltip placement - AB-27

--- src/ui/src/components/shared/SharedFile.vue
+++ src/ui/src/components/shared/SharedFile.vue
@@ -14,6 +14,7 @@
 				variant="tertiary"
 				size="smallIcon"
 				data-writer-tooltip="Download File"
+				data-writer-tooltip-placement="left"
 				:aria-label="`Download ${name || 'File'}`"
 				@click="$emit('download')"
 			>
