bb5ffc2034e4b7deb49901d9577ced61b2fdd739
UladzislauK-Writer <uladzislau.kasabutski@writer.com>
2025-09-05 17:40:33 +0200

    chore: update default template

--- apps/default/main.py
+++ apps/default/main.py
@@ -3,11 +3,14 @@ import writer as wf
 # Shows in the log when the app starts
 # print("Hello world!")
 
+# Or you can use a logger object
+# logger.info("Successful startup")
+
 # You can define functions which can be called from Python code blocks
-def my_func():
-    return 1
+# def my_func():
+#    return 1
 
 # You can initialize state via code
-initial_state = wf.init_state({
-    "my_var": 1337,
-})
+# initial_state = wf.init_state({
+#    "my_var": 1337,
+# })
