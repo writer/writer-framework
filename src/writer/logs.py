c5f6d7d2c180ea0eafc7534bf1b456f55e85bfec
Ramiro Medina <64783088+ramedina86@users.noreply.github.com>
2025-09-17 15:47:34 +0000

    fix: Unserializable dumps

--- src/writer/logs.py
+++ src/writer/logs.py
@@ -190,7 +190,11 @@ class JSONFormatter(logging.Formatter):
             if len(record.args) > 0 and isinstance(record.args[0], dict):
                 data.update(record.args[0])
 
-        return json.dumps(data)
+        try:
+            data_as_json = json.dumps(data)
+            return data_as_json 
+        except Exception:
+            return '{"unserializable": true}' 
 
 
 def get_handler(format: str = WRITER_LOG_FORMAT, level: str = WRITER_LOG_LEVEL):
