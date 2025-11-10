065a2ba463110db3c057a7222a72e02f93c76174
Uzair <uzair@writer.com>
2025-11-10 18:03:07 +0000

    ref: remove temporary PoC app

--- apps/temp-cron-poc/scripts/common.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-BASE_URL = "http://127.0.0.1:5000/private/api"
-TEMPORAL_SERVER_URL = "localhost:7233"
-
-@dataclass
-class JobParams:
-    blueprint_id: str
-    branch_id: str
