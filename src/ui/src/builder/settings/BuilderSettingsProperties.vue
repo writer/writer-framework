8950403cc9fd1c236fa162bf08580d702e7e5c0b
Hakan Baybas <42409653+bybash@users.noreply.github.com>
2025-12-17 16:38:18 +0000

    feat: UX for double click on nodes [AB-879]

--- src/ui/src/builder/settings/BuilderSettingsProperties.vue
+++ src/ui/src/builder/settings/BuilderSettingsProperties.vue
@@ -289,6 +289,7 @@ const secretsManager = inject(injectionKeys.secretsManager);
 
 const expandedFields = ref(new Set());
 
+// Check if the code field should be expanded for the current component
 const isFieldExpanded = computed(() => {
 	return (
 		ssbm.expandedEditorForComponent.value === selectedComponent.value?.id
